0d0531499386b9981d79bf226845ccffd61a9f3b
ocean <849638313@qq.com>
2020-08-06 10:00:01 +0800

    # 目录调整

--- lib/ocean_package/constants.rb
+++ lib/ocean_package/constants.rb
@@ -5,9 +5,9 @@ module OceanPackage
 
     # 有效的 CONFIGURATION 值
     AVAILABLE_CONFIGURATIONS = %w(Debug Release)
-    DEFAULT_CONFIGURATION = 'Debug'
-    DEFAULT_COMPANY_NAME = 'YourCompanyName'
-    DEFAULT_ARCHIVE_PATH = '$HOME/Documents/Ipas/'
+    DEFAULT_CONFIGURATION = "Debug"
+    DEFAULT_COMPANY_NAME = "YourCompanyName"
+    DEFAULT_ARCHIVE_PATH = "#{Dir.home}/Documents/Ipas/"
 
   end
 
--- test/ocean_package_test.rb
+++ test/ocean_package_test.rb
@@ -7,6 +7,8 @@ class OceanPackageTest < Minitest::Test
 
   def test_it_does_something_useful
 
+    puts "#{OceanPackage::Constants::DEFAULT_ARCHIVE_PATH}"
+
     # path = '/Users/ocean/Documents/myipas/ztoExpressClient.ipa'
     # ipa = OceanPackage::Ipa.new(path)
     # ipa.make_tmp_dir
@@ -25,30 +27,30 @@ class OceanPackageTest < Minitest::Test
     # ipa = OceanPackage::Ipa.new(path)
     # ipa.run
 
-    content = "当前平台: iOS\n\n"
-    content += "APP名称: " + "中通快递" + "\n\n"
-    content += "当前版本: " + "5.9.0" + "(1)" + "\n\n"
-    content += "打包耗时: " + "todo" + "\n\n"
-    content += "发布环境: " + "Debug" + "\n\n"
-    content += "更新描述: " + "Bug fixed" + "\n\n"
-    content += "发布时间: " + "time" + "\n\n"
-    content += "下载链接: [点我](http://wuhaiyang.top/f6z1?release_id=5f265b5ab2eb461d4a46d92f)" + "\n\n"
-    content += "![二维码](https://wuhyimages.oss-cn-shanghai.aliyuncs.com/qrcode/5f265b5ab2eb461d4a46d92f)"
-
-    puts "web hook message: \n#{content}"
-
-
-    ding = OceanPackage::DingTalk.new('1d93cc41c8bf5c55ab5a58f1c5119dbca274c9725539b70c9a3025d3e456571d')
-    # ding.send_text_message(content, ['15221047750'], false )
-
-    content_value = "# 中通快递 \n\n" + content
-
-    ding.send_card_message("iOS 来新包啦~", content_value)
-
-    ding.send_text_message("iOS 来新包啦~", ["15221047750"])
-
-
-    time = Time.new
+    # content = "当前平台: iOS\n\n"
+    # content += "APP名称: " + "中通快递" + "\n\n"
+    # content += "当前版本: " + "5.9.0" + "(1)" + "\n\n"
+    # content += "打包耗时: " + "todo" + "\n\n"
+    # content += "发布环境: " + "Debug" + "\n\n"
+    # content += "更新描述: " + "Bug fixed" + "\n\n"
+    # content += "发布时间: " + "time" + "\n\n"
+    # content += "下载链接: [点我](http://wuhaiyang.top/f6z1?release_id=5f265b5ab2eb461d4a46d92f)" + "\n\n"
+    # content += "![二维码](https://wuhyimages.oss-cn-shanghai.aliyuncs.com/qrcode/5f265b5ab2eb461d4a46d92f)"
+    #
+    # puts "web hook message: \n#{content}"
+    #
+    #
+    # ding = OceanPackage::DingTalk.new('1d93cc41c8bf5c55ab5a58f1c5119dbca274c9725539b70c9a3025d3e456571d')
+    # # ding.send_text_message(content, ['15221047750'], false )
+    #
+    # content_value = "# 中通快递 \n\n" + content
+    #
+    # ding.send_card_message("iOS 来新包啦~", content_value)
+    #
+    # ding.send_text_message("iOS 来新包啦~", ["15221047750"])
+    #
+    #
+    # time = Time.new
 
 
   end
